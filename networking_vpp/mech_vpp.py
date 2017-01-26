87fa85f28829575ad30c2385d3b9293264a11c55
Naveen Joy <najoy@cisco.com>
2017-01-26 15:31:15 -0800

    Resolve too many threads running on the etcd and DB journal
    Closes-bug: #1644371
    
    Implement an election algorithm to elect two threads
    as masters for doing work - one for DB journal (forward workers)
    and the other for etcd (reverse workers). The threads that are
    not elected as master go to sleep and check if the master is
    alive periodically. If they cannot detect a master, a new master
    is elected to do the work.
    
    Change-Id: I13a4dbe4e58c8156231acf7fab2ca293f7b4e4cb

--- networking_vpp/mech_vpp.py
+++ networking_vpp/mech_vpp.py
@@ -26,7 +26,10 @@ from oslo_log import log as logging
 import re
 import six
 import time
+import uuid
 
+from agent.utils import EtcdHelper
+from etcdutils import EtcdElection
 from networking_vpp.agent import utils as nwvpp_utils
 from networking_vpp.compat import context as n_context
 from networking_vpp.compat import directory
@@ -445,9 +448,11 @@ class EtcdAgentCommunicator(AgentCommunicator):
         self.state_key_space = LEADIN + '/state'
         self.port_key_space = LEADIN + '/nodes'
         self.secgroup_key_space = LEADIN + '/global/secgroups'
+        self.election_key_space = LEADIN + '/election'
         self.do_etcd_mkdir(self.state_key_space)
         self.do_etcd_mkdir(self.port_key_space)
         self.do_etcd_mkdir(self.secgroup_key_space)
+        self.do_etcd_mkdir(self.election_key_space)
         self.secgroup_enabled = cfg.CONF.SECURITYGROUP.enable_security_group
         if self.secgroup_enabled:
             self.register_secgroup_event_handler()
@@ -463,12 +468,17 @@ class EtcdAgentCommunicator(AgentCommunicator):
             # Newton and on
             ev = events.AFTER_CREATE
 
+        # Clear any previously elected master keys from the election key space
+        EtcdHelper(self.etcd_client).clear_state(self.election_key_space)
         registry.subscribe(self.start_threads, resources.PROCESS, ev)
 
     def start_threads(self, resource, event, trigger):
         LOG.debug('Starting background threads for Neutron worker')
-        self.return_thread = eventlet.spawn(self._return_worker)
-        self.forward_thread = eventlet.spawn(self._forward_worker)
+        # Assign a UUID to each worker thread to enable thread election
+        self.return_thread = eventlet.spawn(self._return_worker,
+                                            str(uuid.uuid4()))
+        self.forward_thread = eventlet.spawn(self._forward_worker,
+                                             str(uuid.uuid4()))
 
     def find_physnets(self):
         physical_networks = set()
@@ -917,12 +927,18 @@ class EtcdAgentCommunicator(AgentCommunicator):
             # Thrown when the directory already exists, which is fine
             pass
 
-    def _forward_worker(self):
+    def _forward_worker(self, thread_id):
         LOG.debug('forward worker begun')
 
         session = neutron_db_api.get_session()
+        etcd_election = EtcdElection(self.etcd_client, 'forward_worker',
+                                     self.election_key_space, thread_id,
+                                     wait_until_elected=True,
+                                     recovery_time=3)
         while True:
             try:
+                etcd_election.wait_until_elected()
+
                 def work(k, v):
                     LOG.debug('forward worker updating etcd key %s', k)
                     if self.do_etcd_update(k, v):
@@ -934,6 +950,12 @@ class EtcdAgentCommunicator(AgentCommunicator):
                         return False
 
                 LOG.debug('forward worker reading journal')
+                # TODO(najoy): Limit the journal read entries processed
+                # by a worker thread to a finite number, say 50.
+                # This will ensure that one thread does not run forever.
+                # The re-election process will wake up one of the sleeping
+                # threads after the specified recovery_time of 3 seconds
+                # and will get a chance to split the available work
                 while db.journal_read(session, work):
                     pass
                 LOG.debug('forward worker has emptied journal')
@@ -969,7 +991,7 @@ class EtcdAgentCommunicator(AgentCommunicator):
 
     ######################################################################
 
-    def _return_worker(self):
+    def _return_worker(self, thread_id):
         """The thread that manages data returned from agents via etcd."""
 
         # TODO(ijw): this should begin by syncing state, particularly
@@ -1024,4 +1046,6 @@ class EtcdAgentCommunicator(AgentCommunicator):
                                     'etcd port feedback: %s', key)
 
         ReturnWatcher(self.etcd_client, 'return_worker',
-                      self.state_key_space, data=self).watch_forever()
+                      self.state_key_space, self.election_key_space,
+                      thread_id, wait_until_elected=True,
+                      recovery_time=3, data=self).watch_forever()
